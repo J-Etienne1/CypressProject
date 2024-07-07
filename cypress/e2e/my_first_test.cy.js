e36e61f8d59f18cc3f183233549daaf5dec24f9a
Jason <jasonetienne1@gmail.com>
2024-07-07 14:15:31 +0100

    updated tes to confirm search results

--- cypress/e2e/my_first_test.cy.js
+++ cypress/e2e/my_first_test.cy.js
@@ -7,16 +7,9 @@ it("google test", function () {
     cy.get('#L2AGLb > .QS5gu').click()
 
     // Adding Timeouts at a command level
-    cy.get('#APjFqb', {timeout: 6000}).type("Cypress{enter}")
-    cy.get('.eKjLze > .g > [lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click()
-
-    // Handle new Origin as moved for Google during the test
-    cy.origin('https://www.cypress.io', () => {
-        cy.get('.osano-cm-denyAll').should('be.visible').then($button => {
-            cy.wrap($button).click()
-        })
-        cy.contains("With Cypress, you can easily create tests for your modern web applications", { timeout: 10000 }).should('be.visible')
-    })
+    cy.get('#APjFqb', {timeout: 6000}).type("stackoverflow{enter}")
+    cy.get('[lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb')
+    cy.get('[lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .notranslate > .q0vns > .CA5RN > :nth-child(1) > .VuuXrf').should("be.visible")
 
 })
 
