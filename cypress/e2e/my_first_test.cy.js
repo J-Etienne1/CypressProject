38767d926a84d4c2ca4c126564d92a8639d02d30
Jason <jasonetienne1@gmail.com>
2024-07-07 14:35:35 +0100

    added test for basic login & page nav and element selection

--- cypress/e2e/my_first_test.cy.js
+++ cypress/e2e/my_first_test.cy.js
@@ -13,3 +13,17 @@ it("google test", function () {
 
 })
 
+it.only("login test", function () {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin")
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123")
+    cy.get('.oxd-button').click()
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click()
+    cy.get('.--visited > .oxd-topbar-body-nav-tab-item > .oxd-icon').click()
+    cy.get('.oxd-topbar-body-nav-tab-link').click()
+    cy.get('.orangehrm-header-container > .oxd-button').click()
+    cy.get('.oxd-button--ghost').click()
+
+
+})
+
