39ab5b18e8e651da6838975a65cd462ed8f81586
Jason <jasonetienne1@gmail.com>
2024-07-07 12:49:37 +0100

    updated test to enter click on the link for the Cypress homepage

--- cypress/e2e/my_first_test.cy.js
+++ cypress/e2e/my_first_test.cy.js
@@ -6,5 +6,5 @@ it("google test", function () {
     cy.visit("https://google.com")
     cy.get('#L2AGLb > .QS5gu').click()
     cy.get('#APjFqb').type("Cypress{enter}")
-
+    cy.get('.eKjLze > .g > [lang="en"] > .tF2Cxc > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click()
 })
