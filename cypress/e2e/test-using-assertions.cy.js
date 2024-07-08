f89b2642fed08d818c1d35d8671525a2114f2655
Jason <jasonetienne1@gmail.com>
2024-07-08 20:56:24 +0100

    adding more assertions

--- cypress/e2e/test-using-assertions.cy.js
+++ cypress/e2e/test-using-assertions.cy.js
@@ -2,8 +2,17 @@
 it("learning assertions", function(){
     cy.visit("https://example.cypress.io/")
     cy.contains("get").click()
-    cy.get('#query-btn', {timeout: 6000}).should("contain","Button").should("have.class", "query-btn").should("be.visible").should("be.enabled")
+    cy.get('#query-btn', {timeout: 6000})
+        .should("contain","Button")
+        .should("have.class", "query-btn")
+        .should("be.visible")
+        .should("be.enabled")
+
+    cy.get("#query-btn").invoke("attr", "id")
+        .should("equal", "query-btn")
 
 
-
+    cy.get("#query-btn")
+        .should("contain","Button")
+        .and("have.class", "query-btn")
 })
