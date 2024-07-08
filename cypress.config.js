764debf0e8550a7378c2e8599af605d29c3dae43
Jason <jasonetienne1@gmail.com>
2024-07-08 20:50:27 +0100

    adding assertions & added watchForFileChanges to config and set to false to stop cypress running tests after every save

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}",
+    watchForFileChanges: false,
   },
 })
--- cypress/e2e/test-using-assertions.cy.js
+++ cypress/e2e/test-using-assertions.cy.js
@@ -1,4 +1,9 @@
 
 it("learning assertions", function(){
     cy.visit("https://example.cypress.io/")
+    cy.contains("get").click()
+    cy.get('#query-btn', {timeout: 6000}).should("contain","Button").should("have.class", "query-btn").should("be.visible").should("be.enabled")
+
+
+
 })
