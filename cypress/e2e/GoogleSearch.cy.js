ce6b1d6be020a326d6f75e2fc076b9e469c9758c
Jason <jasonetienne1@gmail.com>
2024-07-10 20:53:08 +0100

    working on generating HTML reports

---
+++ cypress/e2e/GoogleSearch.cy.js
@@ -0,0 +1,16 @@
+
+
+it("Google search 1st Test", () => {
+    cy.log("Google 1st Test starts")
+    cy.visit("https://google.com")
+    cy.get(".gLFyf").type("Google 1st Test ends")
+    cy.log("Google 1st Test ends")
+})
+
+
+it("Google search 2st Test", () => {
+    cy.log("Google 2st Test starts")
+    cy.visit("https://google.com")
+    cy.get(".gLFyf").type("Google 2st Test ends")
+    cy.log("Google 2st Test ends")
+})
