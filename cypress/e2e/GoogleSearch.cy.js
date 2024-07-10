9cfd533ba065d2acb7e125ba0dfb44670316882c
Jason <jasonetienne1@gmail.com>
2024-07-10 21:38:00 +0100

    execute tests with the terminal command npx cypress run --reporter mochawsome
    Generated reports

--- cypress/e2e/GoogleSearch.cy.js
+++ cypress/e2e/GoogleSearch.cy.js
@@ -3,14 +3,17 @@
 it("Google search 1st Test", () => {
     cy.log("Google 1st Test starts")
     cy.visit("https://google.com")
+    cy.get('#W0wltc > .QS5gu').click()
     cy.get(".gLFyf").type("Google 1st Test ends")
     cy.log("Google 1st Test ends")
+
 })
 
 
 it("Google search 2st Test", () => {
     cy.log("Google 2st Test starts")
     cy.visit("https://google.com")
+    cy.get('#W0wltc > .QS5gu').click()
     cy.get(".gLFyf").type("Google 2st Test ends")
     cy.log("Google 2st Test ends")
 })
